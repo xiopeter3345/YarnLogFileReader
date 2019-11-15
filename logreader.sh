72dbef4aaf01a9df4c0677c3a1af142e7a309c8e
xiopeter3345 <xiopeter3345@outlook.com>
2019-11-15 16:49:55 +0800

    Delete logreader.sh

--- logreader.sh
+++
@@ -1,5 +0,0 @@
-# /bin/bash
-
-export CLASSPATH=/usr/hdp/current/hadoop-client/*:/usr/hdp/current/hadoop-client/client/*:/usr/hdp/current/hadoop-hdfs-client/lib/*:/usr/hdp/current/hadoop-yarn-client/*:/etc/hadoop/conf:./target/YarnLogFileReader-1.0-SNAPSHOT.jar
-
-java YarnLogFileReader.YarnLogFileReader $1

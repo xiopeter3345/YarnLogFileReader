e80d378eb88be6d29992f02fad9af84a7b216ed3
apacbigdata <apacbigdata@outlook.com>
2019-11-19 10:19:27 +0800

    change executor after adding dependencies

--- logreader.sh
+++ logreader.sh
@@ -1,8 +1,5 @@
 # /bin/bash
 
-if [ -d "/usr/hdp/current"]; then
-    export CLASSPATH=/usr/hdp/current/hadoop-client/*:/usr/hdp/current/hadoop-client/client/*:/usr/hdp/current/hadoop-hdfs-client/lib/*:/usr/hdp/current/hadoop-yarn-client/*:/etc/hadoop/conf:./target/YarnLogFileReader-1.0-SNAPSHOT.jar
-else
-    export CLASSPATH=./target/YarnLogFileReader-1.0-SNAPSHOT.jar
-fi
+export CLASSPATH=/etc/hadoop/conf:./target/YarnLogFileReader-1.0-SNAPSHOT-dependencies.jar
+
 java YarnLogFileReader.YarnLogFileReader $1

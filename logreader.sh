951348b9de6a1a63550805b61d1229821f6a3429
apacbigdata <apacbigdata@outlook.com>
2019-12-03 15:10:46 +0800

    add ETW trace jars when running on cluster.

--- logreader.sh
+++ logreader.sh
@@ -4,6 +4,6 @@ if [ ! -f "./target/YarnLogFileReader-1.0-SNAPSHOT-dependencies.jar" ]; then
     mvn package assembly:single
 fi
 
-export CLASSPATH=/etc/hadoop/conf:./target/YarnLogFileReader-1.0-SNAPSHOT-dependencies.jar:/usr/hdp/current/hadoop-hdfs-client/lib/adls2-oauth2-token-provider.jar
+export CLASSPATH=/etc/hadoop/conf:./target/YarnLogFileReader-1.0-SNAPSHOT-dependencies.jar:/usr/hdp/current/hadoop-hdfs-client/lib/adls2-oauth2-token-provider.jar:/usr/hdp/current/failover-controller/lib/*
 
 java YarnLogFileReader.YarnLogFileReader $1
